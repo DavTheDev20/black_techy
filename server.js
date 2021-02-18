72048291a5feedddff6ba36e77fe6ac87d6c9a65
DavTheDev20 <reiddavin@outlook.com>
2021-02-18 14:09:45 -0500

    Added asthetic changes to input bars as well as made width of posts larger.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -13,15 +14,18 @@ const port = process.env.PORT || 8080;
 const password = process.env.ATLAS_PASS;
 
 //Step two in heroku deployment
-mongoose.connect(`mongodb+srv://admin-davin:${password}@cluster0.ol1ih.mongodb.net/black_techyDB?retryWrites=true&w=majority`, {
+mongoose.connect(
+  `mongodb+srv://admin-davin:${password}@cluster0.ol1ih.mongodb.net/black_techyDB?retryWrites=true&w=majority`,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    console.log('Connected to black_techyDB');
+  console.log('Connected to black_techyDB');
 });
 
 app.use(morgan('tiny'));
@@ -32,12 +36,11 @@ app.use('/api', router);
 
 //Step three in heroku deployment
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build/'));
+  app.use(express.static('client/build/'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.listen(port, () => console.log(`Server runnning on port: ${port}`));
-
